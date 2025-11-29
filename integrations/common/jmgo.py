a632099ee33a7e059240189a89c9759fee0c1e97
Kenneth McElvain <kenmcelvain@me.com>
2025-11-29 13:10:28 -0800

    comments, decided to pass <startup>.yaml as arg

--- integrations/common/jmgo.py
+++ integrations/common/jmgo.py
@@ -9,28 +9,46 @@ import pathlib
 import yaml
 
 def main():
-    jm_common.startup(None)
-    mpi_jm.sortjobs()
-    jm_common.printJobs()
+    jm_common.startup(None)  # Scans for and loads job files
+    mpi_jm.sortjobs()        # priority sort.
+    jm_common.printJobs()    # log them to scheduler log file
+
+def usage():
+    print("Usage: python3 -m startjm -lumpsize <allocationsize> -blocksize <blocksize> -py jmgo <startup>.yaml")
+    print("jmgo.py is the scheduler startup script for application integrations")
+    print("  -help   - This message")
+    print("  <startup>.yaml  where <startup> is the configuration file for the batch submission")
 
 def schedInit():
+    '''
+    schedInit will be called by jm_sched after startup.
+    We set up the config file, call the standard startup (jm_common.startup) and
+    then sort and print the jobs to the scheduler log file.
+    On return, the scheduler starts work.
+    '''
     argv = sys.argv
     argc = len(argv)
     i = 1
     print("jmgo: processing args", flush=True)
+    foundyaml = "" # false in if
     while i < argc:
         if argv[i] == '-help' or argv[i] == '--help':
             usage()
             quit()
-        elif argv[i] == '-jy':
+        elif argv[i].endswith(".yaml"):
+            if foundyaml:
+                raise ValueError(f"Found second startup yaml file {foundyaml}, then {argv[i]}")
+            foundyaml = argv[i]
+            # Must be the startup file
             # Option to pass startup yaml as command line option
             i = i + 1
             if i >= argc:
                 raise ValueError("missing path for -jy switch")
             print("jmgo: Found startup yaml file {argv[i]}")
+            # stick in environ to be picked up later
             os.environ['JM_LAT_STARTUP_YAML'] = argv[i]
             i = i + 1
-        else{
-            print(f"   args {i}: {argv[i]}", flush=True)
+        else:
+            print(f"   skipping args {i}: {argv[i]}", flush=True)
             i = i + 1
     main()
